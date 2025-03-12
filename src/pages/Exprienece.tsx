3bc5611fda400157ec021a6da46188d3137eb1b8
Sundararajan s <121149600+sundararajan-git@users.noreply.github.com>
2025-03-12 09:56:52 +0530

    Update Exprienece.tsx

--- src/pages/Exprienece.tsx
+++ src/pages/Exprienece.tsx
@@ -9,7 +9,7 @@ const Exprienece = () => {
         </span>
         <span className="text-xl font-medium mt-4">Resume</span>
         <div className="grid grid-cols-1 sm:grid-cols-2  gap-4 p-4">
-          {Array.from({ length: 6 }).map((i) => {
+          {Array.from({ length: 6 }).map((i:any) => {
             return (
               <div className="bg-base-100 p-6 rounded-lg flex flex-col gap-4" key={i}>
                 <div className="badge badge-outline badge-info rounded-md">
